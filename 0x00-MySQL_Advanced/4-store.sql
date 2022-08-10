b0dfbf064303d624f3cb4d1a44a2c20a30064fac
Lesm <88344019+malu17@users.noreply.github.com>
2022-08-10 11:31:16 +0300

    Delete 4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++
@@ -1,5 +0,0 @@
--- creates a trigger that decreases the quantity of an item after adding a new order
-CREATE TRIGGER buy_trigger
-AFTER INSERT ON orders
-FOR EACH ROW
-UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
